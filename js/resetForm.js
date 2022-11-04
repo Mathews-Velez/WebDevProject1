f63f8ed04611365a49a33e8041a8dd51a63c260b
picotski <onipicotte@gmail.com>
2022-11-04 16:20:59 -0400

    add the reset button js

---
+++ js/resetForm.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.getElementById('resetButton').addEventListener('click', resetForm)
+
+  function resetForm() {
+    document.getElementById('project').querySelector('input').value = ""
+    document.getElementById('owner').querySelector('input').value = ""
+    document.getElementById('title').querySelector('input').value = ""
+    document.getElementById('category').querySelector('select').value = ""
+    document.getElementById('hours').querySelector('input').value = ""
+    document.getElementById('rate').querySelector('input').value = ""
+    document.getElementById('status').querySelector('select').value = ""
+    document.getElementById('description').querySelector('textarea').innerHTML = ""
+  }
+},false)
