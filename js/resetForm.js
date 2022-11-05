4b76bedc0e60a398b241ac8475ffb824e6fd592b
picotski <onipicotte@gmail.com>
2022-11-05 17:33:10 -0400

    did css and cleaned the form validation

--- js/resetForm.js
+++ js/resetForm.js
@@ -6,10 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById('project').querySelector('input').value = ""
       document.getElementById('owner').querySelector('input').value = ""
       document.getElementById('title').querySelector('input').value = ""
-      document.getElementById('category').querySelector('select').value = ""
+      document.getElementById('category').querySelector('select').options = "Pract"
       document.getElementById('hours').querySelector('input').value = ""
       document.getElementById('rate').querySelector('input').value = ""
-      document.getElementById('status').querySelector('select').value = ""
+      document.getElementById('status').querySelector('select').options = "Planned"
       document.getElementById('description').querySelector('textarea').innerHTML = ""
     }
   },false)
